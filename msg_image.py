1654ef08917137d1fda2a64762c0f8d0b80e16db
Kamal Souccar <souccar@umass.edu>
2019-12-06 19:00:59 +0000

    print(msg)

--- msg_image.py
+++ msg_image.py
@@ -5,7 +5,7 @@ def mkMsgImage(pl, obsnum, txt, im, label, color='k') :
     fig = pl.figure(figsize = (10,2))
     pl.subplots_adjust(left=0, right=1, bottom=0, top=1)
     msg = '{label}: {txt}\nObsNum: {obsnum}'.format(label=label, txt=txt, obsnum=obsnum)
-    print msg
+    print(msg)
     pl.annotate(msg, xy=(0.05,0.05), color = color, fontsize=25)
     try:
       pl.tick_params(labelbottom=False, labelleft=False, labelright=False, labeltop=False,
