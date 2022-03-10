385e12f7a08017d420eb54f27e1d71bacf1106fb
kamal <souccar@astro.umass.edu>
2022-03-10 12:42:12 -0500

    fit values > half max

--- m2fit.py
+++ m2fit.py
@@ -124,10 +124,13 @@ class m2fit():
         self.parameters = numpy.zeros((self.n,3))
         self.result_relative = numpy.zeros(self.n)
         self.result_absolute = numpy.zeros(self.n)
+        self.scans_xpos = []
+        self.scans_xpos_all = []
     
     def find_focus(self):
         """Uses data loaded in during creation of this instance to fit focus."""
         if self.m2pos < 0: return
+
         for index in range(self.n):
             ptp = numpy.zeros((3,3))
             ptr = numpy.zeros(3)
@@ -136,17 +139,24 @@ class m2fit():
             I = []
             par = []
             pcor = []
+            mdata_max = numpy.max(self.data[:][index])
+            scan_id_good = 0
             for scan_id in range(self.nscans):
+                self.scans_xpos_all.append(self.m2_position[scan_id])
+                if True and self.data[scan_id][index] < 0.5*mdata_max:
+                    continue
                 I.append(self.data[scan_id][index])
                 par.append(self.m2_position[scan_id])
+                self.scans_xpos.append(self.m2_position[scan_id])
                 pcor.append(self.m2_pcor[scan_id])
                 f[0] = 1.
-                f[1] = par[scan_id]
-                f[2] = par[scan_id]*par[scan_id]
+                f[1] = par[scan_id_good]
+                f[2] = par[scan_id_good]*par[scan_id_good]
                 for ii in range(3):
                     for jj in range(3):
                         ptp[ii][jj] = ptp[ii][jj] + f[ii]*f[jj]
-                    ptr[ii] = ptr[ii] + f[ii]*I[scan_id]
+                    ptr[ii] = ptr[ii] + f[ii]*I[scan_id_good]
+                scan_id_good += 1
             ptpinv = numpy.linalg.inv(ptp)
             self.parameters[index,:] = numpy.dot(ptpinv,ptr)
             if self.parameters[index,2] != 0:
@@ -159,8 +169,10 @@ class m2fit():
 
     def fit_focus_model(self):
         """Uses best fit focus (Z) for each instance to fit linear focus model."""
-        if self.n > 1:
-            xband = [index-0.5*(self.n-1) for index in range(self.n)]
+        xband = [index-0.5*(self.n-1) for index in range(self.n)]
+        print('xbands =', xband)
+        if self.n > 1 and len(xband) > 1:
+            print('fit focus')
             ptp = numpy.zeros((2,2))
             ptr = numpy.zeros(2)
             pta = numpy.zeros(2)
@@ -196,9 +208,10 @@ class m2fit():
             self.focus_slope = relative_focus_fit[1]
             self.fit_rms = rms
         elif self.n > 0:
-            self.relative_focus_fit = self.result_relative[0]
+            print('average focus')
+            self.relative_focus_fit = numpy.mean(self.result_relative)
             self.focus_error = 0
-            self.absolute_focus_fit = self.result_absolute[0]
+            self.absolute_focus_fit = numpy.mean(self.result_absolute)
             self.focus_slope = 0
             self.fit_rms = 0
         if self.m2pos == 0:
