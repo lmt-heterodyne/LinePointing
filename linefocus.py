030a233f3363427e65807336a3dca31e10ccfc5c
kamal <souccar@astro.umass.edu>
2022-03-10 12:41:37 -0500

    update to use json results

--- linefocus.py
+++ linefocus.py
@@ -1,5 +1,6 @@
 import ast
 import sys
+import json
 from lmtlp_reduce_cli import lmtlp_reduce_cli
 from allfocus import allfocus
 
@@ -10,8 +11,16 @@ def linefocus(obsNumList, opt, line_list, baseline_list, tsys, tracking_beam):
     image_files = []
     for i,obsN in enumerate(obsNumList):
       print('look for obsnum', obsN)
-      msg = lmtlp_reduce_cli(None, 0, str(obsN), opt=opt, line_list=line_list, baseline_list=baseline_list, tsys=tsys, tracking_beam=tracking_beam)
-      status,peak,azOff,elOff = msg.decode().split(',')
+      results_str = lmtlp_reduce_cli(None, 0, str(obsN), opt=opt, line_list=line_list, baseline_list=baseline_list, tsys=tsys, tracking_beam=tracking_beam)
+      print(results_str)
+      results_dict = json.loads(results_str)
+      status = results_dict['status']
+      peak = results_dict['pk']
+      azOff = results_dict['x']
+      elOff = results_dict['y']
+      azUserOff = results_dict['az_user_off']
+      elUserOff = results_dict['el_user_off']
+      obsPgm = results_dict['obspgm']
       if int(status) < 0:
         return False
       image_file = 'lmtlp_%s.png'%str(obsN)
@@ -26,6 +35,9 @@ def linefocus(obsNumList, opt, line_list, baseline_list, tsys, tracking_beam):
 
 if __name__ == '__main__':
     opt = 0
+    obsNumList = list(range(94823, 94832+1, 1))
+    print(obsNumList)
+
     if len(sys.argv) > 2:
         try:
             opt = int(sys.argv[1], 0)
@@ -37,8 +49,7 @@ if __name__ == '__main__':
         obsNumList = ast.literal_eval(sys.argv[-1])
     except Exception as e:
         print(e)
-        print('must provide ObsNumList')
-        sys.exit(0)
-        
+        print('using default ObsNumList', obsNumList)
+    
     linefocus(obsNumList, opt=opt, line_list=None, baseline_list=None, tsys=200, tracking_beam=None)
 
