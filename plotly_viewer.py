268a361a4c7d2ed7203717dcd1130440966ca13f
souccar <souccar@astro.umass.edu>
2024-06-13 18:02:08 -0400

    added peak_amplitutde and error to the title of the map() plot

--- plotly_viewer.py
+++ plotly_viewer.py
@@ -1183,6 +1183,9 @@ class PlotlyViewer():
         except:
             pass
         textstr = textstr +('\n Tracking Beam %d'%B.BData.tracking_beam if B.BData.tracking_beam >=0 else '\nTracking Center')
+        peak_amplitude = B.peak_fit_params[np.nonzero(isGood),0]
+        peak_error = B.peak_fit_errors[np.nonzero(isGood),0]
+        textstr = textstr +('   Peak Amplitude %0.2f (%0.2f)'%(peak_amplitude,peak_error))
         if with_matplotlib:
             pl.suptitle('ObsNum %d: %s %s %sGHz\n %s'%(B.obsnum,B.BData.receiver,B.BData.source,B.BData.line_rest_frequency,textstr)) 
             try:
