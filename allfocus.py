e14c0fe21d6dc1d652ab731928ac2e3cda77e108
souccar <souccar@astro.umass.edu>
2025-08-05 15:59:49 -0400

    removed main, use linefocus.py

--- allfocus.py
+++ allfocus.py
@@ -160,20 +160,3 @@ def allfocus(allfocus_list, opt=0):
     allfocus_results_d['pos'] = f.m2pos
     return allfocus_results_d
 
-if __name__ == '__main__':
-    obsNums = [83578, 83579, 83580, 83581, 83582]
-    peaks  = [8.386131542861701, 14.80816494349861, 16.462157307450138, 15.80190338676115, 9.86341460275963]
-    imageFiles = ['lp_spec_83578_1568497923432_47231.png', 'lp_spec_83579_1568497924551_47231.png', 'lp_spec_83580_1568497924985_47231.png', 'lp_spec_83581_1568497925395_47231.png', 'lp_spec_83582_1568497925891_47231.png']
-    opt = 0x1
-    obsNums = [93164, 93165, 93166, 93167, 93168]
-    peaks = [1050.5671629157275, 1159.8579761398939, 1218.4598409124826, 1157.5620061643654, 1043.9765320135932]
-    imageFiles = ['lmtlp_93164_*.png', 'lmtlp_93165_*.png', 'lmtlp_93166_*.png', 'lmtlp_93167_*.png', 'lmtlp_93168_*.png']
-    for i,f in enumerate(imageFiles):
-        for f1 in glob.glob(f):
-            print(f1)
-            imageFiles[i] = f1
-    opt = 0
-    allfocus(obsNums, peaks, imageFiles, opt)
-    
-
-
